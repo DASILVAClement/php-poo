2b116a5db5df2043c7c870bb7c38bc612c3460ac
Da Silva Clement <clementdasilva0@gmail.com>
2024-05-21 13:13:28 +0200

    exercices fini

--- poo/tests/testPhrase.php
+++ poo/tests/testPhrase.php
@@ -2,7 +2,7 @@
 
 require_once "../classes/Phrase.php";
 
-$phrase1 = new Phrase("il pleut demain ");
+$phrase1 = new Phrase("il pleut demain et aussi demain il pleut");
 echo $phrase1->getPhrase();
 echo PHP_EOL;
 echo $phrase1->calculerNombreMot();
@@ -13,4 +13,13 @@ echo $phrase1->positionMot("pleut");
 echo PHP_EOL;
 echo $phrase1->positionMot("demain");
 echo PHP_EOL;
-echo $phrase1->ponctuationPhrase();
+echo $phrase1->determinerTypePhrase();
+echo PHP_EOL;
+echo $phrase1->compterOccurrencesMot("demain");
+echo PHP_EOL;
+echo $phrase1->remplacerMot("demain", "maxime");
+echo PHP_EOL;
+echo $phrase1->reformaterPhrase();
+echo PHP_EOL;
+echo $phrase1->compterVoyelles();
+echo PHP_EOL;
